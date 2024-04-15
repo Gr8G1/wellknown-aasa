3d8a04f06d654dde2e19a91f08e08152ebfb9aba
jwhyun <jwhyun@snack24h.com>
2024-04-15 16:13:30 +0900

    test

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     'https://bpfzl.app.link',
     'https://bpfzl-alternate.app.link',
     'https://bpfzl.test-app.link',
-    'https://bpfzl-alternate.test-app.link'
+    'https://bpfzl-alternate.test-app.link',
     'https://bpfzl.test-app.link?id=test',
     'https://bpfzl-alternate.test-app.link?qr=1234'
   ], [])
