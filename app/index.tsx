31439f7592937bfe34baf6eaa2f430e178e21809
jwhyun <jwhyun@snack24h.com>
2024-04-17 18:54:09 +0900

    test

--- app/index.tsx
+++ app/index.tsx
@@ -4,14 +4,21 @@ import { View, Text, StyleSheet, Linking, Pressable } from 'react-native';
 
 export default function Page() {
   const universalLink = useMemo(() => [
-    'https://bpfzl.app.link/hLaH7BP2PIb' 
+    {
+      link: 'https://bpfzl.app.link/hLaH7BP2PIb',
+      data: 'inviteCode: ZXCASDQWE'
+    },{
+      link: 'https://bpfzl.app.link/vgWDioGLRIb',
+      data: 'inviteCode: QWEASDZXC'
+    }
   ], [])
 
   return (
     <View style={styles.container}>
-      {universalLink.map((link) => (
+      {universalLink.map(({ link, data}) => (
         <Pressable key={link} style={styles.press} onPress={() => Linking.openURL(link)}>
           <Text>{link}</Text>
+          <Text>{data}</Text>
         </Pressable>
       ))}
     </View>
